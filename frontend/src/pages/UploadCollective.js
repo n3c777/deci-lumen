523d8ca88055a3f406cfb692d82bdf2986137590
n3c777 <n3c777@gmail.com>
2025-01-16 20:55:59 -0800

    added frontend validation

--- frontend/src/pages/UploadCollective.js
+++ frontend/src/pages/UploadCollective.js
@@ -3,19 +3,48 @@ import UploadAudio from "../components/UploadAudio";
 import AudioList from "../components/AudioList";
 import ProcessAudio from "../components/ProcessAudio";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function UploadCollective() {
   const [trigger, setTrigger] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
   const [selectedAudioId, setSelectedAudioId] = useState(null);
 
   const navigate = useNavigate();
 
-  const goToIndividual = () => {
-    navigate("/upload-individual");
+  const goToIndividual = async () => {
+    try {
+      const isUploadValid = await checkUpload();
+      if (!isUploadValid) {
+        setErrorMessage("The number of audio tracks must be 1");
+      } else {
+        setErrorMessage("");
+        navigate("/upload-individual");
+      }
+    } catch (error) {
+      console.error("Error in goToIndividual:", error);
+      setErrorMessage("An error occurred while validating the upload.");
+    }
   };
+
   const goToWelcome = () => {
     navigate("/");
   };
+
+  const checkUpload = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/raw_audio_list", {
+        params: { is_individual: false },
+      });
+
+      const numberOfAudioTracks = response.data.length;
+      return numberOfAudioTracks === 1;
+    } catch (error) {
+      console.error("Error fetching audio files:", error);
+      return false;
+    }
+  };
+
   return (
     <div>
       <button onClick={goToWelcome}>Go Back</button>
@@ -33,6 +62,7 @@ function UploadCollective() {
         </div>
       )}
       <button onClick={goToIndividual}>Next</button>
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
     </div>
   );
 }
--- frontend/src/pages/UploadIndividual.js
+++ frontend/src/pages/UploadIndividual.js
@@ -3,9 +3,11 @@ import UploadAudio from "../components/UploadAudio";
 import AudioList from "../components/AudioList";
 import ProcessAudio from "../components/ProcessAudio";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function UploadIndividual() {
   const [trigger, setTrigger] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
   const [selectedAudioId, setSelectedAudioId] = useState(null);
 
   const navigate = useNavigate();
@@ -16,11 +18,38 @@ function UploadIndividual() {
 
   const sendData = async () => {
     try {
-      await processAllAudio();
-      await sendMqttData();
-      navigate("/audio-visual");
+      try {
+        const isUploadValid = await checkUpload();
+        if (!isUploadValid) {
+          setErrorMessage(
+            "The number of audio tracks must be between or equal to 1d & 5"
+          );
+        } else {
+          setErrorMessage("");
+          navigate("/audio-visual");
+          await processAllAudio();
+          await sendMqttData();
+        }
+      } catch (error) {
+        console.error("Error in goToIndividual:", error);
+        setErrorMessage("An error occurred while validating the upload.");
+      }
     } catch (error) {
       console.error("Error in sending data:", error);
+    }
+  };
+
+  const checkUpload = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/raw_audio_list", {
+        params: { is_individual: true },
+      });
+
+      const numberOfAudioTracks = response.data.length;
+      if (numberOfAudioTracks > 1 && numberOfAudioTracks < 6) return true;
+    } catch (error) {
+      console.error("Error fetching audio files:", error);
+      return false;
     }
   };
 
@@ -76,6 +105,7 @@ function UploadIndividual() {
         isIndividual={true}
       />
       <button onClick={sendData}>Send Data</button>
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
     </div>
   );
 }
